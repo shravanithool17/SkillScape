090096bf7f03348b87045d5f5cfe7c320b554b69
shravanithool17 <shravanithool17@gmail.com>
2025-08-27 23:01:36 +0530

    Update TaskDay5.js

--- TaskDay5.js
+++ TaskDay5.js
@@ -5,7 +5,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// Sample student list (you can later fetch from DB)
 const students = [
   { id: 1, name: "Shravani", age: 18, course: "Computer Engineering" },
   { id: 2, name: "Krishna", age: 18, course: "IT Engineering" },
@@ -20,3 +19,4 @@ app.get("/students", (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
