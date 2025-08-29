f46bd0eebe5c22f2ca861ae49b5a3f27c487c66b
shravanithool17 <shravanithool17@gmail.com>
2025-08-29 15:01:34 +0530

    Updated  App.jsx for Day 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,8 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
 
 import React from "react";
 import ProfileCard from "./ProfileCard";
+import Counter from "./Counter";
+import TextPreview from "./TextPreview";
 
 function App() {
   return (
@@ -45,6 +12,15 @@ function App() {
         title="Student & Developer"
         bio="Loves coding, learning React, and exploring backend development."
       />
+
+      {/* Counter Section */}
+      <h1>Counter</h1>
+      <Counter />
+
+      {/* Text Preview Section */}
+      <h1>Live Text Preview</h1>
+      <TextPreview />
+
     </div>
   );
 }
